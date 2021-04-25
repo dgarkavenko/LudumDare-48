24fb12a5ae81014da43429de2d8806b0e722a985
oleg_koposov <o.koposov@tarkov.com>
2021-04-25 21:38:03 +0300

    Ready console

--- Assets/Scripts/UI/UIConsoleScreen.cs
+++ Assets/Scripts/UI/UIConsoleScreen.cs
@@ -14,6 +14,26 @@ public class UIConsoleScreen : MonoBehaviour
     private const char NEW_LINE_SEPARATOR_CHAR = '\n';
     private const string NEW_LINE_SEPARATOR_STRING = "\n";
     private readonly List<string> _allLines = new List<string>(64);
+
+    public async Task ShowSingleLine(string text)
+    {
+        _allLines.Add(text);
+        if (_allLines.Count > _maxLinesCount)
+            _allLines.RemoveAt(0);
+            
+        _tmPro.text = string.Join(NEW_LINE_SEPARATOR_STRING, _allLines);
+        
+        await Task.Yield();
+    }
+
+    public async Task ChangeLastLine(string text)
+    {
+        _allLines[_allLines.Count - 1] = text;
+
+        _tmPro.text = string.Join(NEW_LINE_SEPARATOR_STRING, _allLines);
+        
+        await Task.Yield();
+    }
     
     public async Task ShowText(string text)
     {
@@ -24,15 +44,7 @@ public class UIConsoleScreen : MonoBehaviour
     public async Task ShowText(IEnumerable<string> text)
     {
         foreach (var textLine in text)
-        {
-            _allLines.Add(textLine);
-            if (_allLines.Count > _maxLinesCount)
-                _allLines.RemoveAt(0);
-            
-            _tmPro.text = string.Join(NEW_LINE_SEPARATOR_STRING, _allLines);
-
-            await Task.Yield();
-        }
+            await ShowSingleLine(textLine);
     }
     
     private void Update()
