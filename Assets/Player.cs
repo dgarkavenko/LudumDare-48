9933c234c0d93289170a9406442eb357ee84b1ec
dgarkavenko <d.garkavenko@tarkov.com>
2021-04-26 00:49:36 +0300

    new outline

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -13,10 +13,12 @@ public class Player : MonoBehaviour, IRoomie
     public Vector3 HandsOffset;
     public Vector3 HandsPosition => _fpc.playerCamera.transform.position + _fpc.playerCamera.transform.rotation * HandsOffset;
     public Camera Camera => _fpc.playerCamera;
+    public SelectionOutlineController Outliner;
 
     public void OnValidate()
     {
         _fpc = GetComponent<FirstPersonController>();
+        Outliner = _fpc.playerCamera.GetComponent<SelectionOutlineController>();
     }
 
     public void LateUpdate()
