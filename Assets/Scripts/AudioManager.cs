fcefbc8b4e38d57a45a04de5ebc6be289e8d5c3a
Anton Smetanin <Anton.Smetanin@gmail.com>
2021-04-26 00:19:54 +0900

    Music updated

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -11,6 +11,8 @@ public class AudioManager : MonoBehaviour
     [Range(0, 1)] public float Drums;
     [Range(0, 1)] public float Piano;
     [Range(0, 1)] public float Sax;
+    [Range(0, 1)] public float Rain;
+    [Range(0, 1)] public float Thunder;
 
     public void Start()
     {
@@ -23,5 +25,7 @@ public class AudioManager : MonoBehaviour
         Mixer.SetFloat("Drums", Mathf.Lerp(-80, 0, Drums));
         Mixer.SetFloat("Piano", Mathf.Lerp(-80, 0, Piano));
         Mixer.SetFloat("Sax", Mathf.Lerp(-80, 0, Sax));
+        Mixer.SetFloat("Rain", Mathf.Lerp(-80, 0, Rain));
+        Mixer.SetFloat("Thunder", Mathf.Lerp(-80, 0, Thunder));
     }
 }
